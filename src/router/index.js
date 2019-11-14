93f9644fc2784927f4bc89858b7542a60b405b7c
mauritius2209 <mauritius29lase@gmail.com>
2019-11-14 12:48:35 +0700

    UGD11_VUE_C_9413

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const DashboardLayout = () =>
+    import ( /* webpackChunkName: "dashboard" */
+        '../components/dashboardLayout.vue')
+
+function loadView(view) {
+    return () =>
+        import (
+            /* webpackChunkName: "view-
+            [request]" */
+            `../components/dashboardContents/${view}.vue`)
+}
+const routes = [{
+    path: '/',
+    component: DashboardLayout,
+    children: [{
+        name: 'UserController',
+        path: '',
+        component: loadView('userController')
+    }, {
+        name: 'Cabang Controller',
+        path: '/branch',
+        component: loadView('cabangController')
+    }]
+}, ]
+Vue.use(Router)
+const router = new Router({
+    mode: 'history',
+    routes: routes
+})
+export default router
